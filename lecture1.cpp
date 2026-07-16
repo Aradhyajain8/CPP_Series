eac566e24aee403d726c618d6382ad8f7305a813
Aradhya Jain <146551921+Aradhyajain8@users.noreply.github.com>
2026-07-17 03:25:40 +0530

    lecture 1 and lecture 2

---
+++ lecture1.cpp
@@ -0,0 +1,9 @@
+#include<iostream> //header file
+using namespace std; //namespace declaration
+
+int main(){ //execution begins
+    int n;
+    cin>>n;
+    cout<<"value of n is "<<n;
+    return 0;
+}   
---
+++ lecture2.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // arithmetic operators
+    // int a = 4;
+    // int b = 2;
+
+    // int sum = a+b;
+    // int mul = a*b;
+    // int subs = a-b;
+    // int div = a/b;
+    // int modulus = a%b;
+    // cout<<"sum "<<sum<<endl;
+    // cout<<"subtraction "<<subs<<endl;
+    // cout<<"multiplication "<<mul<<endl;
+    // cout<<"division "<<div<<endl;
+    // cout<<"modulus/remainder "<<modulus<<endl;
+
+    // CONSTANT
+    // const int num = 5;
+    // // num = 6; //error
+    // cout<<num;
+
+    // RELATIONAL
+    // ==,!=,>,<,>=,<=
+    // int a = 5;
+    // int b = 3;
+
+    // cout<<(a==b)<<endl;
+    // cout<<(a!=b)<<endl;
+    // cout<<(a>b)<<endl;
+    // cout<<(a<b)<<endl;
+    // cout<<(a>=b)<<endl;
+    // cout<<(a<=b)<<endl;
+
+
+    // LOGICAL OPERATORS 
+    // AND OR
+    // int age = 28;
+    
+    // cout<<((age>=18) && (age<=25))<<endl;
+    // cout<<((age>=18) || (age<=25));
+
+
+
+    // increment operator (++)
+    // int a = 2;
+    // // int b = a++;  //post increment
+    // // cout<<"b "<<b<<endl;
+    // // cout<<"a"<<a<<endl;
+
+    // // pre increment
+    // int c= ++a;
+    // cout<<"c"<<c<<endl;
+    
+    // decrement operator (--)
+    int a = 2;
+    //  post decrement
+    // int b = a--;
+    // cout<<"a "<<a<<endl;
+    // cout<<"b "<<b<<endl;
+
+    // pre decrement
+    int c = --a;
+    cout<<"a "<<a<<endl;
+    cout<<"c "<<c<<endl;
+}
